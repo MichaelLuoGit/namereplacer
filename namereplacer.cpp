e3009432ff5e84c3d392702a1834efe4c003772f
michaelluogit <16luom@burlingamehighschool.org>
2014-12-05 00:14:35 -0800

    ayy
    lmao 2ez4rtz

---
+++ namereplacer.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+using namespace std;
+int main(int argc, char* argv[])
+{
+	std::ifstream mystream;
+	mystream.open("muhstory.txt");
+	std::string story;
+	std::string nameToReplace(argv[1]);
+	std::string replacingName(argv[2]);
+	std::size_t position;
+
+	while(std::getline(mystream, story))
+	{
+	}
+	while((position = story.find(nameToReplace))!= string::npos)
+	{
+		story.replace(position, nameToReplace.length(), replacingName);
+	}
+	std::cout << story;
+	std::cin.ignore();
+}
